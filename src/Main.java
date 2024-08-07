c84974cb3719414afc7606de2e3cd1f1a52fecad
Tess Gaston Løvgreen <tess@lovegreen.life>
2024-08-07 15:06:35 +0200

    file reading code added to main method

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /** This example demonstrates the foundations of building an object-oriented system in Java.
  *  It uses the domain of a bank
  *
- *  Interactivity branch
- *    The user can now do multiple things:
- *    - add new customers
- *    - see a list of customer
- *    - choose a customer
- *    - update a customer's balance
- *    - end the session
+ *  Persistence branch
+ *    The customer data is now being persisted, meaning that when a session ends,
+ *   the state (name and balance) of all the customer objects gets written to a file
+ *   It also means that when the session starts, the code will look for data
+ *   and recreate customer objects based on that data.
+ *
  *
  *
  *  THINGS TO CONSIDER
- *
- *    A user session will now generate new valuable data,
- *    but when the session ends, this data is gone
- *    How can we persist the state of the program so that next time
- *    we start the program we can continue where we left off?
- *
+ *  The main method is no longer as clean as befor.
+ *  With all the code concerned with persistance, it has become difficult to read.
+ *  Consider how we can refactor by moving this code to a utility class for data persistence - like the one we have for interacting with the user, TextUI
  *
  */
 
@@ -26,7 +26,24 @@ class Main {
     public static void main(String[] args) {
 
         Bank bank = new Bank("Sparekassen");
+        String path ="customers.csv";
+        File file = new File(path);
+        try {
+            Scanner scan = new Scanner(file);
+            scan.nextLine();//skip header
 
+            while(scan.hasNextLine()){
+                String line = scan.nextLine(); // "tess, 40000"
+                String[] values= line.split(",");
+                String name = values[0];
+                int balance = Integer.parseInt(values[1].trim());
+                bank.createCustomer(name,balance);
+
+            }
+
+        }catch(FileNotFoundException e){
+            System.out.println("File was not found");
+        }
 
         bank.runDialog();
 
