dd3675c9784ecf1d5c677d95cc8e8376542ca35c
Tess Gaston Løvgreen <tess@lovegreen.life>
2024-08-13 14:45:37 +0200

    didactic comment

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 /** This example demonstrates the foundations of building an object-oriented system in Java.
  *  It uses the domain of a bank
  *
- *  Refactor_main_FileIO branch
- *      We have cleaned up the main method by
- *      moving all file reading and writing code
- *      to a utility class called FileIO
+ *  Refactor_main_FileIO branch demonstrates the following subjects and techniques
+ *
  *
  *  Main
- *   - call to an endSession method in Bank
+ *   - refactoring by removal of business logic code
+ *  FileIO
+ *
  *  Bank
  *   - conditioned call from constructor to get the data read
  *   - endSession method with a call to FileIO's saveData method
+ *
+ * Things to consider
+ *  We can use this code as boilerplate for any project that uses user dialog and datapersistence.
+ *  For example, we soon need to create a turnbased game, where the player's score must be saved after each session.
+ *  We could start here and start by refactoring the Customer class into a Player class, and the Bank class into a Game class.
  *
  */
 
